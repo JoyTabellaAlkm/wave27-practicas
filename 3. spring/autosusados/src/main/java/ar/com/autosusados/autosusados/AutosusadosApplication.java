c2a9743b0b2c1c1678a7d05b9e762358b9101f08
stephaaniie <stephaanie.castillo@gmail.com>
2024-09-20 10:41:17 -0300

    Se descarga el proyecto de sprint para el ejercicio de autos usados.

---
+++ 3. spring/autosusados/src/main/java/ar/com/autosusados/autosusados/AutosusadosApplication.java
@@ -0,0 +1,13 @@
+package ar.com.autosusados.autosusados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosusadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosusadosApplication.class, args);
+	}
+
+}
---
+++ 3. spring/autosusados/src/test/java/ar/com/autosusados/autosusados/AutosusadosApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.autosusados.autosusados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosusadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
