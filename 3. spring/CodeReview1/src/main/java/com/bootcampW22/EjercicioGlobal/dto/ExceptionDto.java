776d61978490ea40bd03744179a9f74e4c19c882
LJossue <leand_jossue@hotmail.com>
2024-09-23 18:45:14 -0500

    añade code review 1

---
+++ 3. spring/CodeReview1/src/main/java/com/bootcampW22/EjercicioGlobal/dto/ExceptionDto.java
@@ -0,0 +1,13 @@
+package com.bootcampW22.EjercicioGlobal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ExceptionDto {
+    private String message;
+    private Integer status;
+}
