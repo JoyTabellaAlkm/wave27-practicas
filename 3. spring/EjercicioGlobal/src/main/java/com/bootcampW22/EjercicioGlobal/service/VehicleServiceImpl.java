9ed90625eeb3c9b818a9bd6bbb76a078c86694e4
leidy320 <leidy.alcaraz@gmail.com>
2024-09-23 17:43:15 -0500

    se agrega evaluacion CodeReview

---
+++ 3. spring/EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal/service/VehicleServiceImpl.java
@@ -0,0 +1,45 @@
+package com.bootcampW22.EjercicioGlobal.service;
+
+import com.bootcampW22.EjercicioGlobal.dto.VehicleDto;
+import com.bootcampW22.EjercicioGlobal.entity.Vehicle;
+import com.bootcampW22.EjercicioGlobal.exception.NotFoundException;
+import com.bootcampW22.EjercicioGlobal.repository.IVehicleRepository;
+import com.bootcampW22.EjercicioGlobal.repository.VehicleRepositoryImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class VehicleServiceImpl implements IVehicleService{
+
+    IVehicleRepository vehicleRepository;
+
+    public VehicleServiceImpl(VehicleRepositoryImpl vehicleRepository){
+        this.vehicleRepository = vehicleRepository;
+    }
+    @Override
+    public List<VehicleDto> searchAllVehicles() {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Vehicle> vehicleList = vehicleRepository.findAll();
+        if(vehicleList.isEmpty()){
+            throw new NotFoundException("No se encontró ningun auto en el sistema.");
+        }
+        return vehicleList.stream()
+                .map(v -> mapper.convertValue(v,VehicleDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<VehicleDto> listByColorAndYear(String color, int year) {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Vehicle> vehicles = vehicleRepository.findAll();
+
+        if(vehicles.isEmpty()){
+            throw  new NotFoundException("No se encontraron vehiculos con esos criterios");
+        }
+        return vehicles.stream().filter(vehicle -> vehicle.getColor().equalsIgnoreCase(color) && vehicle.getYear() == year)
+                .map(vehicle -> mapper.convertValue(vehicle, VehicleDto.class)).toList();
+    }
+}
