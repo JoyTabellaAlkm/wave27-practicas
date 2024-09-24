4d348176b2022f8ae43ec885e7fe891a41904e3b
mSoledadAmore <msoledadamore@gmail.com>
2024-09-24 15:05:03 -0300

    Delete 3. spring/EjercicioGlobal directory

--- 3. spring/EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal/exception/AlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcampW22.EjercicioGlobal.exception;
-
-public class AlreadyExistException extends RuntimeException {
-    public AlreadyExistException(String message) {
-        super(message);
-    }
-}
--- 3. spring/EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal/exception/InvalidVehicleException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcampW22.EjercicioGlobal.exception;
-
-public class InvalidVehicleException extends RuntimeException {
-    public InvalidVehicleException(String message) {
-        super(message);
-    }
-}
