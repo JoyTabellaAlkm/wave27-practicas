41fb5f49c510c449965948e1eea6b3df666b31d6
fbaldrich_meli <fernando.baldrich@mercadolibre.com>
2024-10-04 13:59:54 -0300

    added testing exercise

---
+++ 5. testing/ecommerce-crud/src/main/java/ar/com/mercadolibre/ecommerce/controllers/ProductController.java
@@ -0,0 +1,39 @@
+package ar.com.mercadolibre.ecommerce.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.mercadolibre.ecommerce.models.Product;
+import ar.com.mercadolibre.ecommerce.service.IProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    IProductService service;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable String id) {
+        Product product = service.getProductById(id);
+        return product != null ? ResponseEntity.ok(product) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return service.createProduct(product);
+    }
+}
---
+++ 5. testing/ecommerce-crud/src/main/java/ar/com/mercadolibre/ecommerce/models/Product.java
@@ -0,0 +1,37 @@
+package ar.com.mercadolibre.ecommerce.models;
+
+public class Product {
+    private String id;
+    private String name;
+    private Double price;
+
+    public Product(String id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
