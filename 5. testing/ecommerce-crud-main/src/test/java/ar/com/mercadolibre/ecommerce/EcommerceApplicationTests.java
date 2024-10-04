80c152ec836b798ee4bfc4526ef7daca2508575d
Carlos-Vasquez11 <cvasquez@frba.utn.edu.ar>
2024-10-04 12:57:42 -0300

    Nueva practica de testing

---
+++ 5. testing/ecommerce-crud-main/src/test/java/ar/com/mercadolibre/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.mercadolibre.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
