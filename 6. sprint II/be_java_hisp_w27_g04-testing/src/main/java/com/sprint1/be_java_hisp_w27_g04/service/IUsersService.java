ae7bf5f1f0a8d223945f0bf3bb75849404f6bc5b
Carlos-Vasquez11 <cvasquez@frba.utn.edu.ar>
2024-10-10 12:21:38 -0300

    Agrego proyecto del Sprint II

---
+++ 6. sprint II/be_java_hisp_w27_g04-testing/src/main/java/com/sprint1/be_java_hisp_w27_g04/service/IUsersService.java
@@ -0,0 +1,15 @@
+package com.sprint1.be_java_hisp_w27_g04.service;
+
+import com.sprint1.be_java_hisp_w27_g04.dto.response.*;
+import com.sprint1.be_java_hisp_w27_g04.entity.User;
+import java.util.List;
+
+
+public interface IUsersService {
+    ResponseFollowersDTO getFollowers(int userId,String order);
+    UserFollowersCountResponseDTO getUserFollowersCountById(int userId);
+    ResponseFollowedDTO getFollowed(int userId, String order);
+    ResponseDTO followSeller(int userId, int userIdToFollow);
+    ResponseDTO unfollowSeller(int userId, int userIdToUnfollow);
+    List<ResponsePostDTO> getSellerPosts(int userId);
+}
