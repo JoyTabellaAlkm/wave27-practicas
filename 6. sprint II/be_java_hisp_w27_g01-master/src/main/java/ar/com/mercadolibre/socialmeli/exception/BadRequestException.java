98748e779e4d948f2e2376a9f7bcbfe8649e99d0
stephaaniie <stephaanie.castillo@gmail.com>
2024-10-10 17:46:39 -0300

    Se agrega el documento tecnico.

--- 6. sprint II/be_java_hisp_w27_g01-master/src/main/java/ar/com/mercadolibre/socialmeli/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package ar.com.mercadolibre.socialmeli.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message){
-        super(message);
-    }
-}
--- 6. sprint II/be_java_hisp_w27_g01-master/src/test/java/ar/com/mercadolibre/socialmeli/unit/repository/RepositoryImplTest.java
+++
@@ -1,4 +0,0 @@
-package ar.com.mercadolibre.socialmeli.unit.repository;
-
-public class RepositoryImplTest {
-}
