218dc06aca2b436678e58cfd4dd2e2e6d536e6c5
abasalo-meli <antonella.basalo@mercadolibre.com>
2024-10-10 18:35:03 -0300

    Se sube repo con los fixes solicitados

--- 6. sprint II/be_java_hisp_w27_g04/src/main/java/com/sprint1/be_java_hisp_w27_g04/repository/impl/PostRepositoryImpl.java
+++ 6. sprint II/be_java_hisp_w27_g04/src/main/java/com/sprint1/be_java_hisp_w27_g04/repository/impl/PostRepositoryImpl.java
@@ -1,12 +1,20 @@
 package com.sprint1.be_java_hisp_w27_g04.repository.impl;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.sprint1.be_java_hisp_w27_g04.entity.Post;
 import com.sprint1.be_java_hisp_w27_g04.entity.User;
 import com.sprint1.be_java_hisp_w27_g04.exceptions.NotFoundException;
 import com.sprint1.be_java_hisp_w27_g04.repository.IPostRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -14,10 +22,39 @@ import java.util.Optional;
 @Repository
 public class PostRepositoryImpl implements IPostRepository {
     List<Post> postList;
-    private Integer actualId = 1;
+    private Integer actualId = 100;
 
     public PostRepositoryImpl() throws IOException {
         postList = new LinkedList<>();
+        loadDataBase();
+    }
+
+    private void loadDataBase() throws IOException {
+
+        try {
+            File file = ResourceUtils.getFile("classpath:posts.json");
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
+
+            // Registrar el módulo para manejar LocalDate
+
+            objectMapper.registerModule(new JavaTimeModule());
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+            postList = objectMapper.readValue(file, new TypeReference<List<Post>>() {
+            });
+            postList.forEach(
+                    post -> {
+                        if (post.getPostId() == 1) {
+                            post.setPostDate(LocalDate.now());
+                        }
+                    }
+            );
+
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error loading character data: " + e.getMessage(), e);
+        }
     }
 
     @Override
