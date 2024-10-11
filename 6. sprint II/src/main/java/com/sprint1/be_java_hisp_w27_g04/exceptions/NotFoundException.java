ab4d065651afff777c6854cae4fd261d77365de8
leidy320 <leidy.alcaraz@gmail.com>
2024-10-11 09:31:33 -0500

    Se realiza sprint ||

--- 6. sprint II/src/main/java/com/sprint1/be_java_hisp_w27_g04/exceptions/NotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.sprint1.be_java_hisp_w27_g04.exceptions;
-
-import java.io.IOException;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message){
-        super(message);
-    }
-}
