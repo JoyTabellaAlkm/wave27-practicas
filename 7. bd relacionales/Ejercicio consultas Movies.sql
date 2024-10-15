73c0fec2e79fafff107bd6775b780082e22a0d36
mSoledadAmore <msoledadamore@gmail.com>
2024-10-15 14:18:18 -0300

    Agrego DER Concesionaria y Consultas Movies

---
+++ 7. bd relacionales/Ejercicio consultas Movies.sql
@@ -0,0 +1,15 @@
+select * from movies;
+select * from actors; 
+select * from series;
+select first_name, last_name, rating from actors;
+select title as 'Titulo' from series;
+select first_name, last_name from actors where rating > 7.5;
+select title, rating, awards from movies where rating > 7.5 and awards > 2;
+select title, rating from movies order by rating;
+select title from movies limit 3;
+select * from movies order by rating desc limit 5;
+select * from actors limit 10;
+select title, rating from movies where title like "Toy Story";
+select * from actors where first_name like "Sam%";
+select title from movies where release_date >= "2004-01-01" and release_date <= "2008-01-01";
+select title from movies where rating > 3 and awards > 1 and release_date >= "1988-01-01" and release_date <= "2009-01-01" order by rating;
