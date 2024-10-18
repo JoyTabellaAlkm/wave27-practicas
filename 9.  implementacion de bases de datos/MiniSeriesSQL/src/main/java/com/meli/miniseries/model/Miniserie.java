8a5dc6e9c513a68c4921c7d079860efe8e86ba99
vanelozano7312 <vane.lozano7312@gmail.com>
2024-10-18 16:04:35 -0500

    Implementacion con H2 y SQL mini Series

---
+++ 9.  implementacion de bases de datos/MiniSeriesSQL/src/main/java/com/meli/miniseries/model/Miniserie.java
@@ -0,0 +1,22 @@
+package com.meli.miniseries.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="mini_series")
+public class Miniserie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private Double rating;
+    @Column(name = "amount_of_awards")
+    private int amountOfAwards;
+}
