f4d31709c1c92ccef866a89b27a5d3c8350ff733
Sofia Montoya <sofia09262001@gmail.com>
2024-10-21 14:27:53 -0500

    Se realiza el ejercicio de QA Testers

--- 9.  implementacion de bases de datos/QATesters/src/main/java/com/mercadoLibre/QATesters/config/exceptions/GlobalHandlerException.java
+++ 9.  implementacion de bases de datos/QATesters/src/main/java/com/mercadoLibre/QATesters/config/exceptions/GlobalHandlerException.java
@@ -1,8 +1,10 @@
 package com.mercadoLibre.QATesters.config.exceptions;
 
 import com.mercadoLibre.QATesters.dto.ResponseDTO;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,5 +14,10 @@ public class GlobalHandlerException {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDTO(HttpStatus.NOT_FOUND, e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDTO(HttpStatus.BAD_REQUEST, e.getAllErrors().stream().map(DefaultMessageSourceResolvable::getDefaultMessage).toList().toString()));
     }
 }
