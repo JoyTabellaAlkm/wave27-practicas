bfa2250cd451223650db08b827e855c548eb70f1
mSoledadAmore <msoledadamore@gmail.com>
2024-10-21 09:41:01 -0300

    Agrego ejercicio Miniseries

---
+++ 9.  implementacion de bases de datos/Ejercicio miniseries/src/main/java/com/mercadolibre/miniseries/repository/IMiniserieRepository.java
@@ -0,0 +1,9 @@
+package com.mercadolibre.miniseries.repository;
+
+import com.mercadolibre.miniseries.entity.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniserieRepository extends JpaRepository<MiniSerie, Long> {
+}
---
+++ 9.  implementacion de bases de datos/Ejercicio miniseries/target/classes/Data.sql
@@ -0,0 +1,11 @@
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('Band of Brothers', 9.5, 6);
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('The Queen’s Gambit', 8.6, 2);
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('Mare of Easttown', 8.5, 4);
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('The Night Of', 8.4, 1);
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('When They See Us', 8.9, 4);
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('The Haunting of Hill House', 8.6, 1);
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('Olive Kitteridge', 8.2, 3);
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('Sharp Objects', 8.1, 2);
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('I May Destroy You', 8.3, 5);
+INSERT INTO mini_serie(name, rating, amount_of_awards) VALUES ('Chernobyl: The Lost Tapes', 9.0, 1);
+
